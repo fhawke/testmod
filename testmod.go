9296b7ad04dbf9e9d75e252aacdc22bc24c48739
罗诗菡 <7142220@qq.com>
2022-04-19 12:04:52 +0800

    v2.0.0

--- testmod.go
+++ testmod.go
@@ -1,5 +1,7 @@
 package testmod
-        import "fmt"
-    func SayHello(name string) string {
-   return fmt.Sprintf("你好, %s", name)
+import (
+    "fmt"
+)
+func SayHello(name, str string) string {
+    return fmt.Sprintf("你好, %s, %s", name, str)
 }
