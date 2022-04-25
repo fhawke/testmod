43c1c88f6109d34af21cbd196e4f334399c53f13
罗诗菡 <7142220@qq.com>
2022-04-25 19:44:45 +0800

    v3 diff

--- testmod.go
+++ testmod.go
@@ -5,6 +5,10 @@ import "fmt"
 func SayHello(name string) string {
 	return fmt.Sprintf("你好, %s", name)
 	//rebase and v2 change: 1.0.0
+<<<<<<< HEAD
 	//v3 dif conflict
+=======
+	//v3 diff
+>>>>>>> ae04d9c (v3 diff)
 	//add v3
 }
